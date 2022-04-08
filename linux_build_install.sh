a7e30c91cb746b137afe02870c5aac943bff1401
geminal <adamantiumgreen@gmail.com>
2022-04-07 18:31:44 -0700

    Done

---
+++ linux_build_install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+pip install -r requirements.txt;
+python -m build;
+raw_ver=$(python -V | sed 's/[^0-9]*//g');
+fil_ver=${raw_ver:0:2};
+pip install ../dist/threedfa-0.0.1-cp${fil_ver}-cp${fil_ver}-$(uname | awk '{print tolower($0)}')_$(arch).whl;
+echo Compiled and installed threedfa
