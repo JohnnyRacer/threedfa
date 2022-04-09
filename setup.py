aa2e480b939ad64a72ac8c5c0880e26687fa59f0
JohnnyRacer <77214388+JohnnyRacer@users.noreply.github.com>
2022-04-09 01:02:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="threedfa",
-    version="0.0.1",
+    version="0.0.2",
     author="JohnnyRacer",
     cmdclass={'build_ext': build_ext},
     include_package_data=True,
