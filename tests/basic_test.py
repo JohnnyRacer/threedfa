96544147ffa7b685fb0dc3fb25009bdd01ed0327
Ubuntu <ubuntu@ip-172-26-12-254.us-west-2.compute.internal>
2022-04-24 01:04:57 +0000

    Removed pypi build upload

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -4,16 +4,10 @@ from PIL import Image
 from skimage import io as skio
 
 nn = Instance()
-
-loaded_img = skio.imread('https://bibliogram.pussthecat.org/imageproxy?url=https%3A%2F%2Fscontent-bos3-1.cdninstagram.com%2Fv%2Ft51.2885-15%2F274536839_332749138780209_3662527826268926236_n.jpg%3Fstp%3Ddst-jpg_e35_p1080x1080%26_nc_ht%3Dscontent-bos3-1.cdninstagram.com%26_nc_cat%3D109%26_nc_ohc%3D5-6tl3CNvYIAX9JMpAZ%26edm%3DAAuNW_gBAAAA%26ccb%3D7-4%26oh%3D00_AT9ZJg9vX1shPoC7q1pWqN6NQe7nYOqGc2LDKWz9h5jZcw%26oe%3D6257F2AF%26_nc_sid%3D498da5')
+loaded_img = skio.imread('https://images.unsplash.com/photo-1646061633551-6e8a1e09b72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80')
 boxes = nn.detect_face(loaded_img)
-
 ver_lst = nn.vlist(loaded_img, boxes, ret_dense=True)
-
 mesh = depth(loaded_img, ver_lst, nn.tddfa.tri)
-
 Image.fromarray(mesh).save('meshoutput_depth.png')
-
 print(mesh)
-
 print(len(ver_lst))
