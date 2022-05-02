5f8b6b355c858d0159e346aee9af496af051589c
geminal <adamantiumgreen@gmail.com>
2022-05-02 01:44:44 -0700

    Fixed tests

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -4,7 +4,7 @@ from PIL import Image
 from skimage import io as skio
 
 nn = Instance()
-loaded_img = skio.imread(f'./media/test_images/1.jpeg ')
+loaded_img = skio.imread(f'./tests/media/test_images/1.jpeg ')
 landmarks = nn.detect_lms(loaded_img, ret_dense=True,round_int=False)
 print(f"Found {len(landmarks)} number of faces ")
 mesh = depth(loaded_img, landmarks, nn.tddfa.tri)
