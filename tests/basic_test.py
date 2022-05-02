22b7763f59e3f6f6b079ae2dd291edd5b9b34c61
geminal <adamantiumgreen@gmail.com>
2022-05-02 01:22:47 -0700

    Corrected the workflow

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -4,10 +4,10 @@ from PIL import Image
 from skimage import io as skio
 
 nn = Instance()
-loaded_img = skio.imread(f'./media/test_results/1.jpeg ')
+loaded_img = skio.imread(f'./media/test_images/1.jpeg ')
 landmarks = nn.detect_lms(loaded_img, ret_dense=True,round_int=False)
 print(f"Found {len(landmarks)} number of faces ")
 mesh = depth(loaded_img, landmarks, nn.tddfa.tri)
 Image.fromarray(mesh).save('meshoutput_depth.png')
-print(mesh)
+
 
