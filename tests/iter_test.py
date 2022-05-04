0140c965b1f767e2696371176ee97bdc57d2e6fa
geminal <adamantiumgreen@gmail.com>
2022-05-03 17:38:49 -0700

    Fixed tests

--- tests/iter_test.py
+++ tests/iter_test.py
@@ -1,5 +1,6 @@
 from hashlib import md5
 import io
+import os
 import shutil
 from threedfa import *
 import cv2
@@ -16,6 +17,7 @@ test_types = ['base', 'depth', 'pncc', 'uvtex']
 hash_dict = {}
 
 for test_t in test_types:
+    os.makedirs('./tests/media/test_results/lms', exist_ok=True)
     for i in range(0,5):
         in_fp = f'./tests/media/test_images/{i+1}.jpeg'
         loaded_img = skio.imread(images_url(i+1))
@@ -28,30 +30,11 @@ for test_t in test_types:
         hash_dict[test_t]['in_fp'] = in_fp
         hash_dict[test_t]['out_fp'] = out_fp
         
-        plotted_lms = [FaceUtils.cv_draw_landmark(loaded_img,lm) for lm in landmarks]
-        for idx, lm in enumerate(landmarks):
-            with open(f'./tests/media/test_results/lms/dense_{i}_{idx}.npy', 'wb') as wb:
-                np.save(wb ,lm)
-        for idx, im in enumerate(plotted_lms):
-            plotted_im = f'./tests/media/test_results/lmdense_{i}_{idx}.png'
-            Image.fromarray(im).save(plotted_im)
 
-        image_out = Image.fromarray(mesh)
-        image_out.save(out_fp)
-        output = run([f"md5sum {out_fp} "] ,shell=True,capture_output=True,text=True)
-        shsum_output = output.stdout.split(' ')[0]
-        hash_dict[test_t]["result_hash"] = shsum_output
-        with io.BytesIO() as wb:
-            image_out.save(wb, 'png')
-            wb.seek(0)
-            img_contents = wb.read()
-        hash_out =  md5(img_contents).hexdigest()
-        print(f"PyHslib {hash_out} | md5sum {shsum_output} ")
-        if hash_out == shsum_output:
-            print("Hash checks out and equals")
         landmarks = nn.detect_lms(loaded_img, ret_dense=False,round_int=False)
         plotted_lms = [FaceUtils.cv_draw_landmark(loaded_img,lm) for lm in landmarks]
         for idx, lm in enumerate(landmarks):
+            print(f"Landmark shape is {lm.shape} ")
             with open(f'./tests/media/test_results/lms/sparse_{i}_{idx}.npy', 'wb') as wb:
                 np.save(wb ,lm)
 
